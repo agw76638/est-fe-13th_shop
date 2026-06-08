64a11b1317ed7af0bf9d368369e14bf02a9ef710
Gunnwook Ahn <agw76638@protonmail.com>
2026-06-08 16:35:49 +0900

    탭 구현

--- js/product.js
+++ js/product.js
@@ -1,4 +1,4 @@
-import { addToCart, updateCartCount } from "./utils/common.js";
+import { addToCart, updateCartCount } from './utils/common.js';
 
 let product = {};
 updateCartCount();
@@ -7,60 +7,90 @@ export async function fetchProduct() {
   //console.log(location.search); //?id=3
   let params = new URLSearchParams(location.search);
   //console.log(params.get("id")); // 3
-  const productID = params.get("id"); //3
+  const productID = params.get('id'); //3
   //console.log(typeof productID);  //string
   if (!productID) {
-    alert("잘못된 접근입니다. 홈으로 이동하겠습니다.");
-    location.href = "./index.html";
+    alert('잘못된 접근입니다. 홈으로 이동하겠습니다.');
+    location.href = './index.html';
   }
   try {
-    const res = await fetch("./data/products.json");
-    if (!res.ok) throw new Error("로딩에 실패했습니다.");
+    const res = await fetch('./data/products.json');
+    if (!res.ok) throw new Error('로딩에 실패했습니다.');
     const data = await res.json();
     console.log(data);
     //조회된 상품정보에서 상품의 id가 productID와 일치하는 요소를 변수 product 할당
     product = data.products.find(p => p.id === Number(productID));
     if (!product) {
-      alert("존재하지 않는 상품입니다.");
-      location.href = "./index.html";
+      alert('존재하지 않는 상품입니다.');
+      location.href = './index.html';
     }
     createContent(product);
     createRecommendLists(data.products, product.category, Number(productID));
   } catch (e) {
     console.log(e);
   } finally {
-    console.log("조회를 종료했습니다.");
+    console.log('조회를 종료했습니다.');
     console.log(product);
   }
 }
 
 function createContent(data) {
-  const title = document.querySelector("#product-title"),
-    category = document.querySelector(".product-category"),
-    desc = document.querySelector(".product-description"),
-    origin_price = document.querySelector(".origin-price"),
-    sale_price = document.querySelector(".sale-price"),
-    discount_rate = document.querySelector(".discount-rate"),
-    mainImage = document.querySelector(".main-image img"),
-    details = document.querySelector("#product-info");
+  const title = document.querySelector('#product-title'),
+    category = document.querySelector('.product-category'),
+    desc = document.querySelector('.product-description'),
+    origin_price = document.querySelector('.origin-price'),
+    sale_price = document.querySelector('.sale-price'),
+    discount_rate = document.querySelector('.discount-rate'),
+    mainImage = document.querySelector('.main-image img'),
+    details = document.querySelector('#product-info');
 
   title.textContent = data.title;
   category.textContent = data.category;
   desc.textContent = data.description;
-  origin_price.textContent = (data.price / (1 - data.discountPercentage / 100)).toFixed(2);
+  origin_price.textContent = (
+    data.price /
+    (1 - data.discountPercentage / 100)
+  ).toFixed(2);
   sale_price.textContent = data.price;
   discount_rate.textContent = data.discountPercentage;
-  mainImage.setAttribute("src", data.images[0]);
-  mainImage.setAttribute("alt", data.title);
+  mainImage.setAttribute('src', data.images[0]);
+  mainImage.setAttribute('alt', data.title);
   details.textContent = data.description;
 }
 
 //상품 상세 tab
-const detail_tab_menus = "";
-const detail_tab_contents = "";
+const detail_tab_menus = document.querySelector('.detail-tabs');
+const detail_tab_menu = document.querySelectorAll('.detail-tabs > a');
+const detail_tab_contents = document.querySelector('.tab-content');
+const detail_tab_content = document.querySelectorAll('.detail-content');
+
+detail_tab_menus.addEventListener('click', e => {
+  const clickedTab = e.target.closest('a');
+  if (!clickedTab) return;
+  e.preventDefault();
+  switchTab(clickedTab);
+});
+
+function switchTab(newTab) {
+  const activePanelId = newTab.getAttribute('href');
+  const activePanel = document.querySelector(activePanelId);
+
+  detail_tab_menu.forEach(button => {
+    button.classList.remove('active');
+  });
+
+  detail_tab_content.forEach(panel => {
+    panel.classList.remove('active');
+  });
+
+  newTab.classList.add('active');
+  activePanel.classList.add('active');
+}
 
 function createRecommendLists(all, category, id) {
-  const recommendList = all.filter(p => p.category === category && p.id !== id).slice(0, 4);
+  const recommendList = all
+    .filter(p => p.category === category && p.id !== id)
+    .slice(0, 4);
   const productHTML = recommendList.map(
     p => `
       <article class="product-card">
@@ -84,20 +114,20 @@ function createRecommendLists(all, category, id) {
     `,
   );
 
-  document.querySelector(".recommend-grid").innerHTML = productHTML.join("");
+  document.querySelector('.recommend-grid').innerHTML = productHTML.join('');
 }
 
 fetchProduct();
 
 //상품 수량 변경하기
-const quantity_control = document.querySelector(".quantity-control");
-const quantity = document.querySelector("#quantity");
+const quantity_control = document.querySelector('.quantity-control');
+const quantity = document.querySelector('#quantity');
 
-quantity_control.addEventListener("click", e => {
-  const btn = e.target.closest("button");
+quantity_control.addEventListener('click', e => {
+  const btn = e.target.closest('button');
   if (!btn) return;
   let currentQty = Number(quantity.value);
-  if (btn.textContent === "-") {
+  if (btn.textContent === '-') {
     if (currentQty > 1) {
       currentQty--;
     }
@@ -108,7 +138,7 @@ quantity_control.addEventListener("click", e => {
 });
 
 //장바구니에 담기
-const addcart = document.querySelector("#addcart");
-addcart.addEventListener("click", () => {
+const addcart = document.querySelector('#addcart');
+addcart.addEventListener('click', () => {
   addToCart(product, Number(quantity.value));
 });
